3bf80650f02d2ee13712882433668eab5a8c2f36
Augsutus287 <augustbrink2009@gmail.com>
2026-05-13 11:01:57 +0200

    Add files via upload

--- MovieBot.py
+++ MovieBot.py
@@ -1,10 +1,11 @@
+# olika frågor som ställs till användaren
 genre = input("What genre do you want to watch? ")
 mood = input("What mood do you want to be in? ")
 age = input("What age rating? 0, 7, 13, 18 ")
 
 
 
-
+# Lagras olika filmer 
 Movie =[
     {   "name": "dead poets society", "genre": "drama", "age": "13", "mood": "coziness" },
     {   "name": "cars", "genre": "kidsmovie", "age": "0", "mood": "nostalgic"},
@@ -15,10 +16,10 @@ Movie =[
     {   "name": "arthur and the invisibles", "genre": "animation", "age": "7", "mood": "adventurous" },
     {   "name": "Super Mario Bros Movie", "genre": "animation", "age": "7", "mood": "adventurous"}
     ]
-
+# En loop som körs tills användaren är nöjd med rekommendationen
 while True:
 
-    
+    #hur boten väljer den bästa filmen baserat på användarens svar
     best_score = 0
     best_movie = ""
     for movie in Movie:
@@ -34,9 +35,11 @@ while True:
     if score > best_score:
         best_score = score
         best_movie = movie["name"]    
-     
+     #rekommenderar filmen baserat på resultatet 
     print("Recomendation:", best_movie)
     print("are you satisfied with the recomendation? yes or no")
+   
+   #om svaret är yes så avslutas loppen.
     answer = input()
     if answer == "yes": 
         break
