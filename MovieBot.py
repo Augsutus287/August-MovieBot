b80ab07418319b52689fb975550f9ec40384c62c
Augsutus287 <augustbrink2009@gmail.com>
2026-05-20 11:01:27 +0200

    Add files via upload

--- MovieBot.py
+++ MovieBot.py
@@ -1,47 +1,79 @@
-# olika frågor som ställs till användaren
-genre = input("What genre do you want to watch? ")
-mood = input("What mood do you want to be in? ")
-age = input("What age rating? 0, 7, 13, 18 ")
+ï»¿
+# olika fragor som stalls till anvandaren 
+
 
 
 
 # Lagras olika filmer 
 Movie =[
-    {   "name": "dead poets society", "genre": "drama", "age": "13", "mood": "coziness" },
-    {   "name": "cars", "genre": "kidsmovie", "age": "0", "mood": "nostalgic"},
-    {   "name": "cars 2", "genre": "kidsmovie", "age": "0", "mood": "nostalgic"},
-    {   "name": "cars 3", "genre": "kidsmovie", "age": "0", "mood": "nostalgic" },
-    {  "name": "saw", "genre": "horror", "age": "18", "mood": "thrill" },
-    {   "name": "Ironman", "genre": "action", "age": "13", "mood": "excited" },
-    {   "name": "arthur and the invisibles", "genre": "animation", "age": "7", "mood": "adventurous" },
-    {   "name": "Super Mario Bros Movie", "genre": "animation", "age": "7", "mood": "adventurous"}
-    ]
-# En loop som körs tills användaren är nöjd med rekommendationen
-while True:
+       # Drama
+    {"name": "Dead Poets Society", "genre": "drama", "age": "13", "mood": "coziness"},
+    {"name": "Forrest Gump", "genre": "drama", "age": "13", "mood": "nostalgic"},
+    {"name": "Good Will Hunting", "genre": "drama", "age": "13", "mood": "coziness"},
 
-    #hur boten väljer den bästa filmen baserat på användarens svar
+    # Kidsmovie
+    {"name": "Cars", "genre": "kidsmovie", "age": "0", "mood": "nostalgic"},
+    {"name": "Toy Story", "genre": "kidsmovie", "age": "0", "mood": "nostalgic"},
+    {"name": "The Lion King", "genre": "kidsmovie", "age": "0", "mood": "nostalgic"},
+
+    # Horror
+    {"name": "Saw", "genre": "horror", "age": "18", "mood": "thrill"},
+    {"name": "It", "genre": "horror", "age": "18", "mood": "thrill"},
+    {"name": "The Conjuring", "genre": "horror", "age": "18", "mood": "thrill"},
+
+    # Action
+    {"name": "Iron Man", "genre": "action", "age": "13", "mood": "excited"},
+    {"name": "John Wick", "genre": "action", "age": "18", "mood": "excited"},
+    {"name": "Top Gun: Maverick", "genre": "action", "age": "13", "mood": "excited"},
+
+    # Animation
+    {"name": "Super Mario Bros Movie", "genre": "animation", "age": "7", "mood": "adventurous"},
+    {"name": "Kung Fu Panda", "genre": "animation", "age": "7", "mood": "adventurous"},
+    {"name": "How to Train Your Dragon", "genre": "animation", "age": "7", "mood": "adventurous"},
+
+    # Comedy
+    {"name": "The Hangover", "genre": "comedy", "age": "18", "mood": "excited"},
+    {"name": "Elf", "genre": "comedy", "age": "7", "mood": "coziness"},
+    {"name": "Home Alone", "genre": "comedy", "age": "7", "mood": "nostalgic"},
+
+    # Thriller
+    {"name": "Gone Girl", "genre": "thriller", "age": "18", "mood": "thrill"},
+    {"name": "Shutter Island", "genre": "thriller", "age": "13", "mood": "thrill"},
+    {"name": "Se7en", "genre": "thriller", "age": "18", "mood": "thrill"},
+
+    # Sci-Fi
+    {"name": "Interstellar", "genre": "scifi", "age": "13", "mood": "adventurous"},
+    {"name": "Inception", "genre": "scifi", "age": "13", "mood": "adventurous"},
+    {"name": "The Matrix", "genre": "scifi", "age": "13", "mood": "excited"},
+]
+# En loop som kors tills anvandaren ar nojd med rekommendationen
+while True:
+    genre = input("What genre do you want to watch? ")
+    mood = input("What mood do you want to be in? ")
+    age = input("What age rating? 0, 7, 13, 18 ")
+    
     best_score = 0
-    best_movie = ""
+    best_movies = []  # â† lista istallet for en film
+    
     for movie in Movie:
         score = 0
-    if movie["genre"] == genre:
-        score += 1
+        if movie["genre"] == genre:
+            score += 1
+        if movie["age"] == age:
+            score += 1
+        if movie["mood"] == mood:
+            score += 1
+        if score > best_score:
+            best_score = score
+            best_movies = [movie["name"]] 
+        elif score == best_score and score > 0:
+            best_movies.append(movie["name"])  
 
-    if movie["age"] == age:
-        score +=1
-    if movie["mood"] == mood:
-        score +=1
-
-    if score > best_score:
-        best_score = score
-        best_movie = movie["name"]    
-     #rekommenderar filmen baserat på resultatet 
-    print("Recomendation:", best_movie)
-    print("are you satisfied with the recomendation? yes or no")
-   
-   #om svaret är yes så avslutas loppen.
+    print("Recommendations:", ", ".join(best_movies[:3]))
+    print("Are you satisfied with the recommendation? yes or no")
+    
     answer = input()
-    if answer == "yes": 
+    if answer == "yes":
         break
 
-    print("KING BOB!")
+    
